a8d3f43177c3ed2a8b87e403c9135a85c545eefc
kratos1700 <hunterstarsa@hotmail.com>
2024-07-22 18:55:23 +0200

    build(App):
    Implementación basica de seleccionar una foto de la galeria y subirla a Firebase Storage en XML

--- app/src/main/java/com/example/firestorage/ui/xml/upload/UploadXmlActivity.kt
+++ app/src/main/java/com/example/firestorage/ui/xml/upload/UploadXmlActivity.kt
@@ -8,12 +8,14 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.firestorage.R
 import com.example.firestorage.databinding.ActivityUploadXmlBinding
+import com.example.firestorage.databinding.DialogImageSelectorBinding
 import dagger.hilt.android.AndroidEntryPoint
 import java.io.File
 import java.text.SimpleDateFormat
@@ -33,6 +35,8 @@ class UploadXmlActivity : AppCompatActivity() {
     private val uploadViewModel: UploadXmlViewModel by viewModels()
 
     private lateinit var uri: Uri  // uri de la foto tomada con la cámara
+
+    // este es el lanzador de la cámara que se encarga de abrir la cámara y tomar la foto y recibir el resultado de la foto tomada
     private var intentCameraLauncher =
         registerForActivityResult(ActivityResultContracts.TakePicture()) {
             // aquí se recibe el resultado de la cámara
@@ -40,6 +44,13 @@ class UploadXmlActivity : AppCompatActivity() {
                 uploadViewModel.uploadSimpleImage(uri)
             }
 
+        }
+
+    private var intentGalleryLauncher =
+        registerForActivityResult(ActivityResultContracts.GetContent()) {
+            if (it != null) {
+                uploadViewModel.uploadSimpleImage(it)
+            }
         }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -66,11 +77,42 @@ class UploadXmlActivity : AppCompatActivity() {
     private fun initListeners() {
 
         binding.fabImage.setOnClickListener {
-            takePicture()
+            // takePicture()
+            showImageDialog()
 
         }
 
     }
+
+    private fun showImageDialog() {
+        // aquí se infla el layout del dialogo que se mostrará para seleccionar la imagen de la galería o tomar una foto con la cámara
+        val dialogBinding = DialogImageSelectorBinding.inflate(layoutInflater)
+
+        // aquí se configuran los listeners de los botones del dialogo
+        val dialog = AlertDialog.Builder(this).apply {
+            setView(dialogBinding.root)
+        }.create()
+
+        dialogBinding.btnTakePhoto.setOnClickListener {
+            takePicture()
+            dialog.dismiss() // cerrar el dialogo
+        }
+        dialogBinding.btnGallery.setOnClickListener {
+            getImageFromGallery()
+            dialog.dismiss() // cerrar el dialogo
+        }
+
+        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent) // quitar el fondo del dialogo para que no se vea el borde
+
+        // mostrar el dialogo
+        dialog.show()
+    }
+
+    private fun getImageFromGallery() {
+        // aquí se lanza el intent para seleccionar una imagen de la galería y se recibe el resultado en el callback de la variable intentGalleryLauncher
+        intentGalleryLauncher.launch("image/*")
+    }
+
 
     private fun generateUri() {
         uri = FileProvider.getUriForFile(
@@ -91,6 +133,6 @@ class UploadXmlActivity : AppCompatActivity() {
 
         val name: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date()) + "image"
 
-     return   File.createTempFile(name, ".jpg", externalCacheDir)
+        return File.createTempFile(name, ".jpg", externalCacheDir)
     }
 }
