74eab18c01b6ea2cbc6212a49a95981d41be9bbb
kratos1700 <hunterstarsa@hotmail.com>
2024-07-27 19:41:28 +0200

    build(App):
    Implementación añadir titulo en la foto tomada en camara en Compose

--- app/src/main/java/com/example/firestorage/ui/compose/upload/UploadComposeActivity.kt
+++ app/src/main/java/com/example/firestorage/ui/compose/upload/UploadComposeActivity.kt
@@ -10,6 +10,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -20,14 +21,17 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -37,10 +41,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.core.content.FileProvider
@@ -88,6 +97,7 @@ class UploadComposeActivity : AppCompatActivity() {
     }
 
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun UploadScreen() {
 
@@ -100,6 +110,10 @@ class UploadComposeActivity : AppCompatActivity() {
         var resultUri: Uri? by remember { mutableStateOf(null) } // uri de la imagen subida a Firebase Storage  y descargada de nuevo para mostrarla en la vista de composición
 
         val loading: Boolean by uploadViewModel.isLoading.collectAsState() // variable para mostrar un indicador de carga en la vista de composición
+        var userTitle: String by remember { mutableStateOf("") } // variable para el título de la imagen subida
+
+        val focusRequester = remember { FocusRequester() }
+        val focusManager = LocalFocusManager.current
 
         // este es el lanzador de la cámara que se encarga de abrir la cámara y tomar la foto y recibir el resultado de la foto tomada
         val intentCameraLauncher =
@@ -107,6 +121,8 @@ class UploadComposeActivity : AppCompatActivity() {
                 // aquí se recibe el resultado de la cámara
                 if (it && uri?.path?.isNotEmpty() == true) {
                     uploadViewModel.uploadAndGetImage(uri!!) { newUri ->
+                        userTitle = ""
+                        focusManager.clearFocus()
                         resultUri = newUri
                     } // aquí se sube la imagen a Firebase Storage y se obtiene la url de descarga de la imagen subida
                 }
@@ -132,7 +148,7 @@ class UploadComposeActivity : AppCompatActivity() {
                         // Botón para seleccionar una imagen de la camara
                         OutlinedButton(
                             onClick = {
-                                uri = generateUri() // se genera la uri para la foto
+                                uri = generateUri(userTitle) // se genera la uri para la foto
                                 intentCameraLauncher.launch(uri!!) // se lanza la cámara
                                 showImageDialog = false
                             },
@@ -185,7 +201,11 @@ class UploadComposeActivity : AppCompatActivity() {
 
                 if (resultUri != null) {
 
-                    AsyncImage(model = resultUri, contentDescription = "imagen de subida del user", contentScale = ContentScale.Crop)
+                    AsyncImage(
+                        model = resultUri,
+                        contentDescription = "imagen de subida del user",
+                        contentScale = ContentScale.Crop
+                    )
                 }
 
                 if (loading) { // si hay una carga en progreso se muestra un indicador de carga en la vista de composición
@@ -211,6 +231,34 @@ class UploadComposeActivity : AppCompatActivity() {
 
 
             }
+            Spacer(modifier = Modifier.weight(1f))
+
+            TextField(
+                value = userTitle,
+                onValueChange = { userTitle = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(36.dp)
+                    .border(2.dp, color = colorResource(id = R.color.green), RoundedCornerShape(22))
+                    .focusRequester(focusRequester),
+
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.White,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+
+                    ),
+                maxLines = 1,
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Done
+                )
+
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
             Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
                 FloatingActionButton(onClick = {
                     showImageDialog = true  // se muestra el dialogo de selección de imagen
@@ -228,25 +276,28 @@ class UploadComposeActivity : AppCompatActivity() {
         }
 
 
-
-
     }
 
 
 // Esta funcion es diferenta a la de XML, ya que en compose no se puede usar el metodo createFile() directamente, por lo que se debe de hacer de otra manera
     // en este caso se crea un archivo temporal y se obtiene su URI para poder usarlo en la cámara y subirlo a Firebase Storage después de tomar la foto
 
-    private fun generateUri(): Uri {
+    private fun generateUri(userTitle: String): Uri {
         return FileProvider.getUriForFile(
             Objects.requireNonNull(this),
             "com.example.firestorage.provider",
-            createFile()
+            createFile(userTitle)
         )
     }
 
-    private fun createFile(): File {
+    private fun createFile(userTitle: String): File {
 
-        val name: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date()) + "image"
+        val name: String = userTitle.ifEmpty {
+
+            SimpleDateFormat("yyyyMMdd_HHmmss").format(Date()) + "image"
+        }
+
+
 
         return File.createTempFile(name, ".jpg", externalCacheDir)
     }
